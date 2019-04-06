0a007fc52cd24d9090582f6bdc579cabc35dc5ba
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-06 22:05:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,6 +80,7 @@ def all_holidays_with_bbq(holiday_hash)
     holidays.collect do |holiday, supplies|
        if supplies.include?("BBQ")
          names << holiday
+       end
     end
   end
   names
